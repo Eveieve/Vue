1879a89ae48cc1e53f431e0aa354f07fede1b6c2
Eveieve <jhkdev1@gmail.com>
2025-05-07 16:24:11 +0900

    Complete calculator

--- vue.js/calculator/src/App.vue
+++ vue.js/calculator/src/App.vue
@@ -6,15 +6,88 @@ export default {
       prev :  null, 
       cur : null, 
       operator : null,
-    }
+      operatorActions : {
+        // 사칙 연산 수행하는 함수를 객체로 정의 
+        '+' : (a,b) => a + b, 
+        '-' : (a,b) => a -b,
+        '*' : (a, b) => a * b, 
+        '/' : (a, b) => a / b, 
+      },
+    };
   }, 
 
   methods : {
-    operation(e) { // e is the event 
-      console.log('clicked');
+    // 사용자 입력값에 따라 계산하는 가장 큰 흐름 분기 함수
+    operation(e) {
       const n = e.currentTarget.value;
-      console.log(n);
+      if( n === 'C') {
+        this.clear(); // clear 
+      } else if (['+', '-', '*', '/', '='].includes(n)) {
+        this.calculate(n); 
+      } else {
+        this.saveUserInput(n);
+
+      }
+    },
+
+    // 사용자 입력값을 저장 
+    saveUserInput(n) {
+      // 아무 숫자도 입력하지 않은(null) 상태면 n 저장하기 
+      // null 이 아니라면 숫자뒤에 이어붙이기 (덧셈 아님)
+      this.cur = this.cur === null ? n : (this.cur +=n);
+      this.output = this.cur; 
+    },
+    // 초기화 
+    clear() {
+      this.output = null;
+      this.prev = null;
+      this.cur = null;
+      this.operator = null;
+    }, 
+
+    // 실제 계산하기 
+    calculate(n) {
+      // 입력한 값이 연산자인 경우
+      // 숫자를 입력하지 연산자 클릭한 경우 
+      if(!this.cur && !this.prev) {
+        alert('You need to input a number first');
+        return;
+      }
+
+      // 사칙 연산 기호를 연속으로 클릭 
+      if (this.operator !== '' && !this.cur) {
+        return;
+      }
+      // 등호 클릭 후 한번 더 클릭한 경우 
+      if(n === '=' && this.prev === this.cur) {
+        return;
+      }
+     
+      if (this.operator !== null) {
+        return;
+      }
+
+      this.cur = Number(this.cur); // normal case - change to number 
+      if(this.operator !== null) {
+        this.prev = this.operatorActions[this.operator](this.prev, this.cur);
+        // 등호면 연산 결과 노출 
+        if (n === '=') {
+          this.output = this.prev;
+          this.operator = null;
+          this.cur = this.prev;
+        } else {
+          this.output = null;
+          this.operator = n;
+          this.cur = null;
+        }
+      } else {
+        this.output = null;
+        this.operator = n;
+        this.prev = this.cur;
+        this.cur = null;
+      }
     }
+
   },
 }
 </script>
